b34a925174012f21a0bcf349fea12154ec4fb169
Pedroqueiroz2 <pedro.queiroz@academico.ufpb.br>
2026-03-22 18:18:21 -0300

    feat: adicionando funções nas regras

--- regras.pl
+++ regras.pl
@@ -7,7 +7,21 @@ pode_cursar(Disciplina, Cursadas) :-
     findall(Pre, pre_req(Disciplina, Pre), Pres),
     subset(Pres, Cursadas).
 
+faltam_pre_requisitos(Disciplina, Cursadas, Faltantes) :-
+    findall(Pre, pre_req(Disciplina, Pre), Pres),
+    subtract(Pres, Cursadas, Faltantes).
+
 subset([], _).
 subset([H|T], L) :-
     member(H, L),
     subset(T, L).
+
+% NOVO: lista todas que pode cursar
+todas_podem(Cursadas, Possiveis) :-
+    findall(D,
+        (pre_req(D, _); \+ pre_req(D, _)), % pega todas disciplinas
+        Todas),
+    list_to_set(Todas, Unicas),
+    findall(D2,
+        (member(D2, Unicas), pode_cursar(D2, Cursadas)),
+        Possiveis).
